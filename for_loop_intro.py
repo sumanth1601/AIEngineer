aa80475febd69aad14055da525272a951d2fe7ea
Sumanth Palepu <YOUR_GITHUB_EMAILinboxtosumanth@gmail.com>
2026-07-24 21:21:07 +0530

    Day3: Python Loops

---
+++ for_loop_intro.py
@@ -0,0 +1,6 @@
+b = "ABCDEFGHIJKLMNO"
+
+for i in range(1, 6):
+    for j in range(i):
+        print(chr(65 + j), end=" ")
+    print()
---
+++ while_loop_practice.py
@@ -0,0 +1,10 @@
+while True:
+    print("Enter the Password:")
+    password = input()
+
+    if password == "python123":
+        print("Login Successful")
+        break
+
+    else:
+        print("Wrong Password")
